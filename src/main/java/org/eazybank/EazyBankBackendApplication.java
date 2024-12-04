37136d80dfc924c9e160c6d11ebd64f3bde3a12a
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-12-04 19:29:15 +0100

    various remarks regarding optional annotations for scope of project

--- src/main/java/org/eazybank/EazyBankBackendApplication.java
+++ src/main/java/org/eazybank/EazyBankBackendApplication.java
@@ -2,8 +2,14 @@ package org.eazybank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @SpringBootApplication
+//@EnableJpaRepositories("com.eazybytes.repository") these optional since the @SpringBootApplication is in main folder
+//@EntityScan("com.eazybytes.entities")
 public class EazyBankBackendApplication {
 
     public static void main(String[] args) {
