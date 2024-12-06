46f9ef42b4057f352ee9f5a46b6022e52a1ca94b
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-12-06 12:13:02 +0100

    added ExceptHandling to globally handle an exception

--- src/main/java/org/eazybank/Configuration/ProjectSecurityConfig.java
+++ src/main/java/org/eazybank/Configuration/ProjectSecurityConfig.java
@@ -9,7 +9,10 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import java.nio.file.AccessDeniedException;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -24,10 +27,11 @@ public class ProjectSecurityConfig {
                         .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards").authenticated()
                         .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
         http.formLogin(withDefaults());
-        http.httpBasic(hbc->hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.httpBasic(hbc -> hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
+        return http.build();
 //      http.exceptionHandling(ehc ->ehc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
 //      http.exceptionHandling would be used to set the authenticationEntryPoint globally
-        return http.build();
     }
 
 //    @Bean
