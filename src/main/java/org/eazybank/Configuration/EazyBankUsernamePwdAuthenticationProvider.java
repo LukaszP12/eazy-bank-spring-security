d9485f0e836f02e30bb18bef7c79052d6d14c02e
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-12-06 09:10:04 +0100

    seperate settings corresponding to default and prod profiles

--- src/main/java/org/eazybank/Configuration/EazyBankUsernamePwdAuthenticationProvider.java
+++ src/main/java/org/eazybank/Configuration/EazyBankUsernamePwdAuthenticationProvider.java
@@ -2,6 +2,7 @@ package org.eazybank.Configuration;
 
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
